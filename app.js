0592b5ec8b9d83134d62b34bdb3430562c5cbd9c
Ares Chang <areschang@Aress-Air.ht.home>
2023-04-06 23:00:45 -0400

    change

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const port = process.env.PORT;
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 app.set('views', path.join(__dirname, 'views'));
 
 //* Connect to MongoDB
